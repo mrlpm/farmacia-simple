2ec11d6d4e5affe8ed7564ecde929419aa155d9e
Luis Pérez <luis.perez@protonmail.com>
2020-11-05 18:47:58 -0600

    first commit

---
+++ src/main/java/com/mycompany/farmacia/simple/Farmacia.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.farmacia.simple;
+
+/**
+ *
+ * @author Monica Ranchos y Luis Pérez
+ */
+public class Farmacia {
+    public static void main(String[] args) {
+        Login frmLogin = new Login();
+        frmLogin.setVisible(true);
+    }
+}
