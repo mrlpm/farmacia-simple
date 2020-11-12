e93ad8c8eac1d11ab085b5be10fc23432f61af20
Luis Pérez <luis.perez@protonmail.com>
2020-11-12 00:11:12 -0600

    agregados metodos y formato

--- src/main/java/com/mycompany/farmacia/simple/controladores/Operaciones.java
+++ src/main/java/com/mycompany/farmacia/simple/controladores/Operaciones.java
@@ -40,52 +40,61 @@ import javax.swing.table.DefaultTableModel;
 public class Operaciones {
 
     Connection conn;
-    
-    public Operaciones(Connection cn){
+
+    public Operaciones(Connection cn) {
         this.conn = cn;
     }
-    
-    public ArrayList<String> getPerfiles(){
+
+    public String getUsuario(String usuario) {
+        String query = "SELECT nombres FROM empleados where usuario='" + usuario + "';";
+        String empleado = "";
+        try {
+            Statement stmt = this.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                empleado = rs.getString("nombres");
+            }
+        } catch (SQLException e) {
+        }
+        return empleado;
+    }
+
+    public ArrayList<String> getPerfiles() {
         String query = "SELECT nombre FROM perfiles";
         ArrayList<String> perfiles = new ArrayList();
         try {
             Statement stmt = this.conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
-            while(rs.next()){
+            while (rs.next()) {
                 perfiles.add(rs.getString("nombre"));
-            }
-        } catch (Exception e) {
-        }
-        return perfiles;
-    }
-    
-    public void consultaGenerica(DefaultTableModel modelo, String query){
-        Object[] values = null;
-        try {
-            Statement stmt = this.conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            ResultSetMetaData resultSetMetaData = rs.getMetaData();
-            final int columnCount = resultSetMetaData.getColumnCount();
-            while (rs.next()){
-                values = new Object[columnCount];
-                for (int i = 1; i <= columnCount; i++) {
-                    values[i - 1] = rs.getObject(i);
-                }
-                modelo.addRow(values);
             }
         } catch (SQLException e) {
         }
+        return perfiles;
     }
-    
-    public void ConsultarTabla(DefaultTableModel modelo, String table, String columnas){
-        String query = "SELECT "+columnas+" FROM "+table+";";
+
+    public ArrayList<String> getProveedores() {
+        String query = "SELECT nombre FROM proveedores";
+        ArrayList<String> proveedores = new ArrayList();
+        try {
+            Statement stmt = this.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                proveedores.add(rs.getString("nombre"));
+            }
+        } catch (SQLException e) {
+        }
+        return proveedores;
+    }
+
+    public void consultaGenerica(DefaultTableModel modelo, String query) {
         Object[] values = null;
         try {
             Statement stmt = this.conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
             final int columnCount = resultSetMetaData.getColumnCount();
-            while (rs.next()){
+            while (rs.next()) {
                 values = new Object[columnCount];
                 for (int i = 1; i <= columnCount; i++) {
                     values[i - 1] = rs.getObject(i);
@@ -96,7 +105,26 @@ public class Operaciones {
         }
     }
 
-    public void ConsultarTablaEmpleados(DefaultTableModel modelo){
+    public void ConsultarTabla(DefaultTableModel modelo, String table, String columnas) {
+        String query = "SELECT " + columnas + " FROM " + table + ";";
+        Object[] values = null;
+        try {
+            Statement stmt = this.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData resultSetMetaData = rs.getMetaData();
+            final int columnCount = resultSetMetaData.getColumnCount();
+            while (rs.next()) {
+                values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = rs.getObject(i);
+                }
+                modelo.addRow(values);
+            }
+        } catch (SQLException e) {
+        }
+    }
+
+    public void ConsultarTablaEmpleados(DefaultTableModel modelo) {
         //String query = "SELECT "+columnas+" FROM "+table+";";
         String query = "SELECT p.pk_empleado,p.nombres,p.apellidos,p.usuario,rb.nombre"
                 + " FROM empleados p INNER JOIN perfiles rb ON p.fk_perfil = rb.pk_perfiles;";
@@ -107,7 +135,7 @@ public class Operaciones {
             ResultSet rs = stmt.executeQuery(query);
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
             final int columnCount = resultSetMetaData.getColumnCount();
-            while (rs.next()){
+            while (rs.next()) {
                 values = new Object[columnCount];
                 for (int i = 1; i <= columnCount; i++) {
                     values[i - 1] = rs.getObject(i);
@@ -117,9 +145,9 @@ public class Operaciones {
         } catch (SQLException e) {
         }
     }
-    
-    public void UltimoRegistro(DefaultTableModel modelo, String table, String PK){
-        String query = "SELECT * FROM "+table+" ORDER BY "+PK+" DESC LIMIT 1;";
+
+    public void UltimoRegistro(DefaultTableModel modelo, String table, String PK) {
+        String query = "SELECT * FROM " + table + " ORDER BY " + PK + " DESC LIMIT 1;";
         System.out.println(query);
         Object[] values = null;
         try {
@@ -127,7 +155,7 @@ public class Operaciones {
             ResultSet rs = stmt.executeQuery(query);
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
             final int columnCount = resultSetMetaData.getColumnCount();
-            while (rs.next()){
+            while (rs.next()) {
                 values = new Object[columnCount];
                 for (int i = 1; i <= columnCount; i++) {
                     values[i - 1] = rs.getObject(i);
@@ -137,34 +165,9 @@ public class Operaciones {
         } catch (SQLException e) {
         }
     }
-    public DefaultTableModel Buscar(ArrayList<String> columnas, String table,String criterio, String objeto){
-        String query = "SELECT * FROM "+table+" where "+criterio+" like '%"+objeto+"%';";
-        System.out.println(query);
-        DefaultTableModel modelo = new DefaultTableModel();
-        for (Object columna : columnas) {
-            modelo.addColumn(columna);
-        }
-        Object[] values = null;
-        try {
-            Statement stmt = this.conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            ResultSetMetaData resultSetMetaData = rs.getMetaData();
-            final int columnCount = resultSetMetaData.getColumnCount();
-            while (rs.next()){
-                values = new Object[columnCount];
-                for (int i = 1; i <= columnCount; i++) {
-                    values[i - 1] = rs.getObject(i);
-                    System.out.println(values[i-1]);
-                }
-                modelo.addRow(values);
-            }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e);
-        }
-        return modelo;
-    }
-    
-    public DefaultTableModel buscarGenerico(ArrayList<String> columnas, String query){
+
+    public DefaultTableModel Buscar(ArrayList<String> columnas, String table, String criterio, String objeto) {
+        String query = "SELECT * FROM " + table + " where " + criterio + " like '%" + objeto + "%';";
         System.out.println(query);
         DefaultTableModel modelo = new DefaultTableModel();
         for (Object columna : columnas) {
@@ -176,11 +179,11 @@ public class Operaciones {
             ResultSet rs = stmt.executeQuery(query);
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
             final int columnCount = resultSetMetaData.getColumnCount();
-            while (rs.next()){
+            while (rs.next()) {
                 values = new Object[columnCount];
                 for (int i = 1; i <= columnCount; i++) {
                     values[i - 1] = rs.getObject(i);
-                    System.out.println(values[i-1]);
+                    System.out.println(values[i - 1]);
                 }
                 modelo.addRow(values);
             }
@@ -189,19 +192,45 @@ public class Operaciones {
         }
         return modelo;
     }
-    
-    public void Insertar(String query, String objeto){
+
+    public DefaultTableModel buscarGenerico(ArrayList<String> columnas, String query) {
+        System.out.println(query);
+        DefaultTableModel modelo = new DefaultTableModel();
+        for (Object columna : columnas) {
+            modelo.addColumn(columna);
+        }
+        Object[] values = null;
+        try {
+            Statement stmt = this.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData resultSetMetaData = rs.getMetaData();
+            final int columnCount = resultSetMetaData.getColumnCount();
+            while (rs.next()) {
+                values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = rs.getObject(i);
+                    System.out.println(values[i - 1]);
+                }
+                modelo.addRow(values);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return modelo;
+    }
+
+    public void Insertar(String query, String objeto) {
         try {
             Statement stmt = conn.createStatement();
             int n = stmt.executeUpdate(query);
             if (n != 0) {
                 JOptionPane.showMessageDialog(null, objeto);
             }
-        }catch (HeadlessException | SQLException e){
+        } catch (HeadlessException | SQLException e) {
             JOptionPane.showMessageDialog(null, e);
         }
     }
-    
+
     public void actualizarRegistro(String query, String mensaje) {
         try {
             Statement stmt = conn.createStatement();
@@ -209,7 +238,7 @@ public class Operaciones {
             if (n != 0) {
                 JOptionPane.showMessageDialog(null, mensaje);
             }
-        }catch (HeadlessException | SQLException e){
+        } catch (HeadlessException | SQLException e) {
             JOptionPane.showMessageDialog(null, e);
         }
     }
